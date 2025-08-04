26b271623b1dc5de2ccf8fdc92d70e233b8eb2ef
Milkiyas <milkiyashailub@gmail.com>
2025-08-04 19:46:08 +0300

    feat_redux_setup

--- group66_8/src/lib/redux/types/auth.d.ts
+++
@@ -1,8 +0,0 @@
-type LoginResponse = {
-  token: string | null;
-  userEmail: string | null;
-  userName: string | null;
-  id: string | null;
-  isAuthenticated: boolean;
-  error: string | null;
-};
