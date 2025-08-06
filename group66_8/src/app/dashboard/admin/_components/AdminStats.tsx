7e808799d6faf64edb4847e03c6ff6a06a846252
Milkiyas <milkiyashailub@gmail.com>
2025-08-06 20:09:22 +0300

    feat: admin dashboard

---
+++ group66_8/src/app/dashboard/admin/_components/AdminStats.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import React from 'react';
+import { useGetCycleQuery } from '@/lib/redux/api/cycleApi';
+import { useGetusersQuery } from '@/lib/redux/api/userApi';
+import { Cycle } from '@/lib/redux/types/cycle';
+import { CardComponent } from './CardComponent';
+
+const AdminStats = () => {
+    const { data: cycleData } = useGetCycleQuery();
+    const { data: users = [] } = useGetusersQuery();
+
+    const getTotalUsers = () => users.length;
+
+    const getTotalActiveCycles = () => {
+        return cycleData?.data?.cycles?.filter((cycle: Cycle) => cycle.is_active).length || 0;
+    };
+
+    const getTotalApplicants = () => {
+        return 42;
+    };
+
+    const stats = [
+        { title: 'Total Users', num: getTotalUsers() },
+        { title: 'Total Applicants', num: getTotalApplicants() },
+        { title: 'Active Cycles', num: getTotalActiveCycles() },
+    ];
+
+    return (
+        <div className="flex justify-center py-3">
+            <div className="flex flex-col items-start gap-4">
+                <h1 className="text-2xl  font-bold">Admin Command Center</h1>
+
+                <div className="flex gap-8 flex-wrap justify-center">
+                    <CardComponent title="Total Users" description={125} gradient="purple" />
+                    <CardComponent title="Total Applicants (G7)" description={1204} gradient="green" />
+                    <CardComponent title="Active Cycles" description={1} gradient="orange" />
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default AdminStats;
